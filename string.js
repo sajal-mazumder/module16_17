3c0f7829abb733f4ccc8447cc4da456689d0857e
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-30 22:06:29 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,10 @@
+
+
+// different types of declaring string
+
+let name = ' Sajal';
+let address = "Sandwip";
+let homeDistrict = `Chattagram`;
+let homeDivision = new String('Chattagram');
+console.log(typeof homeDivision);
+console.log(homeDivision);
